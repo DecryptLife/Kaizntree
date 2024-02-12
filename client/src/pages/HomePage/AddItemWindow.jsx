808cdb70c1a5a7dfd4e40b0e06f9bda198b806f1
DecryptLife <benson.thomas2602@gmail.com>
2024-02-12 02:59:21 -0600

    fixing tag&categories - id

--- client/src/pages/HomePage/AddItemWindow.jsx
+++ client/src/pages/HomePage/AddItemWindow.jsx
@@ -2,24 +2,55 @@ import { useEffect, useState } from "react";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { modalState } from "../../store/atoms/model";
 import axios from "axios";
-import BASE_URL from "../../../config";
+import Select from "react-select";
+import tagState from "../../store/atoms/tags";
+import categoriesState from "../../store/atoms/categories";
 
 const AddItemWindow = () => {
-  const url = (path) => `${BASE_URL}${path}`;
   const setModalState = useSetRecoilState(modalState);
-  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useRecoilState(tagState);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [categories, setCategories] = useRecoilState(categoriesState);
   const [formData, setFormData] = useState({
     sku: "",
     email: "",
     category: "",
+    tags: [],
     // ... add more fields as necessary
   });
+
+  const tagOptions = tags.map((tag) => ({ value: tag.id, label: tag.tag }));
+  const categoryOptions = categories.map((category) => ({
+    value: category.id,
+    label: category.category,
+  }));
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
+    }));
+  };
+
+  const handleTagChange = (selectedOptions) => {
+    // Map the selectedOptions to their values (i.e., tag IDs)
+    const selectedTagValues = selectedOptions
+      ? selectedOptions.map((option) => option.value)
+      : [];
+
+    setSelectedTags(selectedTagValues); // Update the state with all selected tag IDs
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      tags: selectedTagValues, // Update formData with all selected tag IDs
+    }));
+  };
+
+  const handleCategoryChange = (selectedOption) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      category: selectedOption ? selectedOption.value : "",
     }));
   };
 
@@ -31,18 +62,8 @@ const AddItemWindow = () => {
   };
 
   useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const response = await axios.get(url("/api/category/"));
-        console.log(response.data);
-        setCategories(response.data);
-      } catch (error) {
-        console.error("Error fetching categories: ", error);
-      }
-    };
-
-    fetchCategories();
-  }, []);
+    console.log("Updated: ", formData);
+  }, [formData]);
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
@@ -68,30 +89,42 @@ const AddItemWindow = () => {
             className="block w-full p-2 border rounded mb-3"
           />
 
-          <select
+          {/* <select
             multiple
-            type="text"
             name="tags"
-            value={formData.tags}
-            onChange={handleInputChange}
+            value={selectedTags}
+            onChange={handleTagChange}
             placeholder="Tags"
-            className="block w-full p-2 border rounded mb-3"
-          />
-
-          <select
-            type="select"
-            name="category"
-            value={formData.category}
-            onChange={handleInputChange}
-            placeholder="Category"
-            className="block w-full p-2 border rounded mb-3"
+            className="form-multiselect block w-full p-2 border rounded mb-3 mt-1"
           >
-            {categories.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.category}
+            {tags.map((tag) => (
+              <option id={tag.id} key={tag.id} value={tag.id}>
+                {tag.tag}
               </option>
             ))}
-          </select>
+          </select> */}
+
+          <Select
+            isMulti
+            name="tags"
+            options={tagOptions}
+            className="basic-multi-select"
+            classNamePrefix="select"
+            onChange={handleTagChange}
+            value={tagOptions.filter((option) =>
+              selectedTags.includes(option.value)
+            )}
+          ></Select>
+          <Select
+            name="category"
+            options={categoryOptions}
+            onChange={handleCategoryChange}
+            placeholder="Category"
+            value={categoryOptions.find(
+              (option) => option.value === formData.category
+            )}
+            className="block w-full mt-2 border rounded mb-3"
+          />
 
           <input
             type="text"
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,42 @@
 import Sidebar from "./Sidebar";
 import Summary from "./Summary";
 import ItemList from "./ItemList";
+import { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+import tagState from "../../store/atoms/tags";
+import categoriesState from "../../store/atoms/categories";
+import axios from "axios";
+import BASE_URL from "../../../config";
 
 const HomePage = () => {
+  const url = (path) => `${BASE_URL}${path}`;
+  const setTags = useSetRecoilState(tagState);
+  const setCategories = useSetRecoilState(categoriesState);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get(url("/api/category/"));
+        console.log(response.data);
+        setCategories(response.data);
+      } catch (error) {
+        console.error("Error fetching categories: ", error);
+      }
+    };
+
+    const fetchTags = async () => {
+      try {
+        const response = await axios.get(url("/api/tags/"));
+        console.log(response.data);
+        setTags(response.data);
+      } catch (error) {
+        console.error("Error fetching tags");
+      }
+    };
+
+    fetchCategories();
+    fetchTags();
+  }, []);
   return (
     <div className="w-full flex">
       <div className="w-1/5 h-screen bg-gray-800 text-white">
